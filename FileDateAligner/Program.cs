4c7828143082ec047f2d9d54174c0e681beebab0
tzengshinfu <tzengshinfu@gmail.com>
2024-01-12 13:36:43 +0800

    整理顯示屬性邏輯

--- FileDateAligner/Program.cs
+++ FileDateAligner/Program.cs
@@ -4,7 +4,8 @@ using Microsoft.WindowsAPICodePack.Shell.PropertySystem;
 using System.Reflection;
 using System.Windows.Forms.VisualStyles;
 
-var fileInfo = ShellFile.FromFilePath("\\\\personalcloud.local\\Public\\miki_ipad\\相機膠捲\\IMG_0127.JPG");
+//var fileInfo = ShellFile.FromFilePath("\\\\personalcloud.local\\Public\\miki_ipad\\相機膠捲\\IMG_0127.JPG");
+var fileInfo = ShellFile.FromFilePath("D:\\Users\\y1938\\Desktop\\gitignore.txt");
 
 //Console.WriteLine($"FileName: {oFileInfo.Name}");
 
@@ -12,13 +13,33 @@ var properties = fileInfo.Properties.DefaultPropertyCollection;
 //oFileInfo.Properties.GetProperty()
 foreach (var property in properties)
 {
-    if (property.PropertyKey.FormatId == new Guid("46802c11-ada9-41b7-8ebe-65ba6699358b"))
+    var key = new Func<string>(() =>
     {
-        Console.WriteLine($"{("Media_FormatCompliance")}=>{(property.ValueAsObject == null ? "無" : property.ValueAsObject.ToString())}");
-    }
-    else
-    {
-        Console.WriteLine($"{(property.Description.DisplayName == null ? property.CanonicalName : property.Description.DisplayName)}=>{(property.ValueAsObject == null ? "無" : property.ValueAsObject.ToString())}");
-    }
+        if (property.PropertyKey.FormatId == new Guid("46802c11-ada9-41b7-8ebe-65ba6699358b"))
+        {
+            return "Media_FormatCompliance";
+        }
+        else if (property.PropertyKey.FormatId == new Guid("9e5e05ac-1936-4a75-94f7-4704b8b01923"))
+        {
+            return "UnknownProperty";
 
+        }
+        else if (property.Description.DisplayName == null)
+        {
+            return property.CanonicalName;
+        }
+
+        return property.Description.DisplayName;
+    })();
+    var value = new Func<string>(() =>
+    {
+        if (property.ValueAsObject == null)
+        {
+            return "無";
+        }
+
+        return property.ValueAsObject.ToString();
+    })();
+
+    Console.WriteLine($"{key}=>{value}");
 }
