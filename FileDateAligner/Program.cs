5ba872cb5b3db5ebeee8ad63e69a54435a070bf9
tzengshinfu <tzengshinfu@gmail.com>
2024-01-17 14:11:20 +0800

    暫存

--- FileDateAligner/Program.cs
+++ FileDateAligner/Program.cs
@@ -1,62 +1,44 @@
 ﻿using Microsoft.WindowsAPICodePack.Shell;
-using Microsoft.WindowsAPICodePack.Shell.PropertySystem;
-
-using System.Reflection;
 using System.Text.RegularExpressions;
-using System.Windows.Forms.VisualStyles;
 
-//var fileInfo = ShellFile.FromFilePath("\\\\personalcloud.local\\Public\\miki_ipad\\相機膠捲\\IMG_0127.JPG");
-//var fileInfo = ShellFile.FromFilePath("\\\\usun-filesvr.usuntek.com\\TW-資訊系統部\\資訊系統部內共用分享\\03.個人文件\\y1938\\image_20240115_133546.png");
-//var fileInfo = ShellFile.FromFilePath("\\\\usun-filesvr.usuntek.com\\TW-資訊系統部\\資訊系統部內共用分享\\20220721-SmartIT會議.mp4");
-var fileInfo = ShellFile.FromFilePath("\\\\personalcloud.local\\Public\\Liu 的 iPad (001416616837)\\相機膠卷\\IMG_0260.MOV");
-var fileName = fileInfo.Name;
-var regex = new Regex("(\\d{4}).{0,1}(\\d{2}).{0,1}(\\d{2}).{0,1}(\\d{2}).{0,1}(\\d{2}).{0,1}(\\d{2})");
+var fileInfo = ShellFile.FromFilePath("D:\\Users\\y1938\\Documents\\Desktop\\IMG_0127.JPG");
+var lastWriteTime = File.GetLastWriteTime(fileInfo.Path);
 
-
-//Console.WriteLine($"FileName: {oFileInfo.Name}");
-
-var properties = fileInfo.Properties.DefaultPropertyCollection;
-//oFileInfo.Properties.GetProperty()
-foreach (var property in properties)
+var dateFinder = new Regex("(\\d{4}).{0,1}(\\d{2}).{0,1}(\\d{2}).{0,1}(\\d{2}).{0,1}(\\d{2}).{0,1}(\\d{2})");
+var findResult = dateFinder.Match(fileInfo.Name);
+if (findResult.Success)
 {
-    var key = new Func<string>(() =>
+    var fileTime = DateTime.Parse(findResult.Groups[1].Value + "/" + findResult.Groups[2].Value + "/" + findResult.Groups[3].Value + " " + findResult.Groups[4].Value + ":" + findResult.Groups[5].Value + ":" + findResult.Groups[6].Value);
+    if (fileTime < lastWriteTime)
     {
-        if (property.PropertyKey.FormatId == new Guid("46802c11-ada9-41b7-8ebe-65ba6699358b"))
-        {
-            return "Media_FormatCompliance";
-        }
-        else if (property.PropertyKey.FormatId == new Guid("9e5e05ac-1936-4a75-94f7-4704b8b01923"))
-        {
-            return "UnknownProperty";
-
-        }
-        else if (property.Description.DisplayName == null)
-        {
-            return property.CanonicalName;
-        }
-
-        return property.Description.DisplayName;
-    })();
-    var value = new Func<string>(() =>
-    {
-        if (property.ValueAsObject == null)
-        {
-            return "無";
-        }
-
-        return property.ValueAsObject.ToString();
-    })();
-
-    if (property.ValueType.ToString().Contains("System.DateTime")) {
-Console.WriteLine($"{"property.Description.DisplayName"}=>{property.Description.DisplayName}");
-Console.WriteLine($"{"property.CanonicalName"}=>{property.CanonicalName}");
-    Console.WriteLine($"{key}=>{value}");
-    //property.Description.DisplayName=>拍攝日期
-    //property.CanonicalName=>System.Photo.DateTaken
-    //拍攝日期=>2013/6/15 下午 06:34:12
-    //property.Description.DisplayName=>建立的媒體
-    //property.CanonicalName=>System.Media.DateEncoded
-    //建立的媒體=>2013/4/22 下午 06:04:33
-    //(\d{4}).{0,1}(\d{2}).{0,1}(\d{2}).{0,1}(\d{2}).{0,1}(\d{2}).{0,1}(\d{2})
+        File.SetLastWriteTime(fileInfo.Path, fileTime);
     }
+}
+else
+{
+    var mediaTime = GetMediaTime(fileInfo, ["System.Photo.DateTaken", "System.Media.DateEncoded"]);
+    if (mediaTime < lastWriteTime && mediaTime > DateTime.Parse("2000/01/01"))
+    {
+        File.SetLastWriteTime(fileInfo.Path, mediaTime);
+    }
+}
+
+static DateTime GetMediaTime(ShellFile fileInfo, String[] propertyKeys)
+{
+    var times = new List<DateTime> { DateTime.MaxValue };
+    var properties = fileInfo.Properties.DefaultPropertyCollection;
+
+    foreach (var propertyKey in propertyKeys)
+    {
+        var time = DateTime.MaxValue;
+        var timeProperty = properties.Where(p => p.CanonicalName == propertyKey).SingleOrDefault();
+        if (timeProperty != null && timeProperty.ValueAsObject != null)
+        {
+            time = DateTime.Parse(timeProperty.ValueAsObject.ToString());
+            times.Add(time);
+        }
+    }
+
+    var mediaTime = times.Min();
+    return mediaTime;
 }
